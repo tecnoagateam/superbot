4004c69cb99091ea23372d455f12e0d345acb315
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-18 19:49:09 +0400

    Update admin.py

--- homebot/core/admin.py
+++ homebot/core/admin.py
@@ -7,8 +7,8 @@ def user_is_admin(user_id):
 	of the approved user IDs.
 	"""
 	if str(user_id) not in get_config("CI_APPROVED_USER_IDS").split():
-		LOGI(f"Access denied to user {user_id}")
+		LOGI(f"İstifadəçiyə giriş rədd edildi {user_id}")
 		return False
 
-	LOGI(f"Access granted to user {user_id}")
+	LOGI(f"İstifadəçiyə giriş {user_id}")
 	return True
